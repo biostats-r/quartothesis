6f12d29a0b4471ccb024487ffa9dff7141d6dd87
richardjtelford <richard.telford@bio.uib.no>
2022-04-28 21:29:53 +0200

    frontpage OK

--- make_thesis.R
+++ make_thesis.R
@@ -6,10 +6,9 @@ library("quarto")
 quarto_render(output_format = "html")
 
 
-
 #pdf
+#install.packages("tinytex")
+#tinytex::install_tinytex() install tinytex for processing LaTeX files
 
 quarto_render(output_format = "pdf")
-
-
 
